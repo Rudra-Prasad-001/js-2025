87af2fb1fb35ffd6f4e94d82f6b7e8be30827efd
Rudra Prasad Pati <148573027+Rudra-Prasad-001@users.noreply.github.com>
2024-12-24 06:51:07 +0000

    Variables revisited

---
+++ JS_basics/01variables.js
@@ -0,0 +1,15 @@
+const customerId = 123;
+let name = "Rudra";
+var status = true;
+city = "Jajpur";
+
+let state;
+
+/* Never use var 
+beacause it does not follow block scope i.e {...} &
+functional scope
+
+Also do not use variables without let keyword
+*/
+
+console.table([customerId, state, name,  status, city]);
