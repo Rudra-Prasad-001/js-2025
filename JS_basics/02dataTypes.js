22d384171a9b377aff04acbda0c7a02c1333ea92
Rudra Prasad Pati <148573027+Rudra-Prasad-001@users.noreply.github.com>
2024-12-24 07:13:12 +0000

    datatypes revisited

---
+++ JS_basics/02dataTypes.js
@@ -0,0 +1,14 @@
+//Primitive datatypes
+let num = 1;            
+let myName = "Rudra"
+let emailIsValid = true;
+let gender;
+let marks = null;  //when something is empty
+let componet = Symbol(); //unique identification
+
+//alert(3+3);  //We are in node not in browser so it gives error
+
+console.table([typeof num,typeof myName,typeof emailIsValid,typeof gender ,typeof marks, typeof componet]); //typeof null => object
+
+
+
