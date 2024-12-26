c5923cb7175803be6984a8d8d322d744a7c8fe30
Rudra Prasad Pati <148573027+Rudra-Prasad-001@users.noreply.github.com>
2024-12-26 17:42:55 +0000

    Learned higher order array functions, Map and it's importance , also learned about for of , for each and for in loops

---
+++ JS_basics/14HigherOrderFunction&Maps.js
@@ -0,0 +1,63 @@
+//Maps hold key value pairs but not duplicates(it is more safer and useful in many scenarios)
+
+let countriesAndCapitals = new Map();
+
+countriesAndCapitals.set("India", "New Delhi");
+
+countriesAndCapitals.set("Pakistan", "Islamabad");
+
+countriesAndCapitals.set("USA", "Washington DC");
+
+countriesAndCapitals.set("China", "Bejing");
+
+countriesAndCapitals.set("Thailand", "Bangkok");
+
+console.log(countriesAndCapitals);
+
+//Iterate Map using for of loop
+const countries = [];
+const capitals = [];
+for (const [country , capital] of countriesAndCapitals) {
+    countries.push(country);
+    capitals.push(capital);
+}
+
+console.log(countries);
+console.log(capitals);
+
+//For of loops for array
+const fruits = ["apple" , "orange" , "mango" , "cherry" , "banana", "jack fruit", "grapes",
+"guava", "coconut", "kiwi" , "berry"
+]
+
+let fruitsStartingWithb = []
+for (const fruit of fruits) {
+    if(fruit[0] == 'b') {
+        fruitsStartingWithb.push(fruit);
+    } 
+}
+
+console.log(fruitsStartingWithb);
+
+//For Each loop for array
+
+fruits.forEach((item, index , array) => {
+    console.log(`item at ${index} of ${array} is ${item}`)});
+
+// We can't use for of loop for object (this will give error)
+
+//Iterate object with for in loop(specially designed for object)
+
+const users = [{name: "Rudra" , age: 21},
+               {name: "Abinash" , age:21},
+               {name: "Somyajeet", age:20}] 
+
+for (const key in users) {
+    console.log(`${users[key].name} is ${users[key].age} years old`);
+}
+
+
+
+
+
+
