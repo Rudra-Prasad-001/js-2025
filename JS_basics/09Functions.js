17b41630a421a4b00d7c364739e7ba89a836b10b
Rudra Prasad Pati <148573027+Rudra-Prasad-001@users.noreply.github.com>
2024-12-26 03:59:52 +0000

    Learned how to work with object (passing them in function) and arrays

--- JS_basics/09Functions.js
+++ JS_basics/09Functions.js
@@ -1,23 +1,45 @@
-function add(a,b) {
-    console.log(a+b); //Returns undefined
+// function add(a,b) {
+//     console.log(a+b); //Returns undefined
+// }
+
+// console.log(add(1,2));
+
+// //Using restOperator (wraps multiple values into an array)
+
+// function pricesInTheCart (...prices) {
+//     return prices;
+// }
+
+// console.log(pricesInTheCart(100,320,400,800,20,60,78,1,98,50));
+
+// let result = (a,b) => {   //Anonymous function
+//     return a*b;
+// }
+
+// console.log(result(3,2));
+
+//Working with objects
+
+const user = {
+    username: "Legend7377",
+    email: "something@gmail.com",
+    age: 21,
+    isLoggedIn: true
 }
 
-console.log(add(1,2));
-
-//Using restOperator (wraps multiple values into an array)
-
-function pricesInTheCart (...prices) {
-    return prices;
+function greetUser(obj) {
+    if(obj.isLoggedIn === true) {
+      return `User ${obj.username} is just arrived!`
+    }
 }
 
-console.log(pricesInTheCart(100,320,400,800,20,60,78,1,98,50));
+console.log(greetUser(user));
 
-let result = (a,b) => {   //Anonymous function
-    return a*b;
+//Working with array
+
+function printSecond(array) {
+    return console.log(`second: ${array[1]}`);
 }
 
-console.log(result(3,2));
-
-
-
-
+let res = printSecond([1,0,3,4]);
+console.log(res); //Will print undefined(bcz console.log() returns undefined)
